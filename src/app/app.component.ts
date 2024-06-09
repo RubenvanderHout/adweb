5e17f7e4549917bea37ec6e0cb80d729890f544f
RubenvanderHout <ruben.vanderhout@gmail.com>
2024-06-09 16:25:41 +0200

    Navigation works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from "@angular/router";
+import { NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 import { HeaderComponent } from './core/layout/header/header.component';
 import { FooterComponent } from './core/layout/footer/footer.component';
+import { filter } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet, FooterComponent],
+  imports: [HeaderComponent, RouterOutlet, FooterComponent, RouterLink, RouterLinkActive],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'adweb';
+  routeName: string = '';
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.setRouteName();
+    });
+  }
+
+  setRouteName() {
+    const currentRoute = this.router.routerState.snapshot.root.firstChild;
+    if (currentRoute?.data) {
+      this.routeName = currentRoute.data['name'] || '';
+    }
+  }
+
+  currentRouteName() {
+    return this.routeName;
+  }
 }
--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component } from "@angular/core";
-import { NavigationEnd, Router } from "@angular/router";
+import { NavigationEnd, Router, RouterLink, RouterLinkActive } from "@angular/router";
 import { filter } from "rxjs";
 @Component({
   selector: "app-layout-header",
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.css",
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   standalone: true,
 })
 export class HeaderComponent {
